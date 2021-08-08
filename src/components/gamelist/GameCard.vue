0fea23fe01e4bf452fd0e5cb27575a63ce1c8035
Shashwat Chandra <chandra.shashwat@gmail.com>
2021-08-08 15:51:41 +0530

    Bugfix

--- src/components/gamelist/GameCard.vue
+++ src/components/gamelist/GameCard.vue
@@ -73,7 +73,7 @@ export default {
         return false;
       }
 
-      if (this.gameData.players.length >= this.gameData.totalPlayers) {
+      if (this.gameData.players.length > this.gameData.totalPlayers) {
         return false;
       }
 

0396a8cbfef1dd8666d918e8b4d09042feaf1d96
Shashwat Chandra <chandra.shashwat@gmail.com>
2021-08-13 17:57:48 +0530

    fix bug in util.js

--- src/util/util.js
+++ src/util/util.js
@@ -7,7 +7,7 @@ function ensureArray (elem) {
         return elem
     }
 
-    let maxV = Math.max(...Object.keys(elem))
+    let maxV = Math.max(...Object.keys(elem)) + 1
     let newElem = new Array(maxV)
     for (let i = 0; i < maxV; i++) {
         newElem[i] = elem[i] || null
